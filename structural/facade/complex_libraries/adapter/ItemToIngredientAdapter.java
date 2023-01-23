2a981da9000ebc52f1af6851babdb3eae2eab793
Stan <stasostaszewski@gmail.com>
2023-01-23 21:27:53 +0100

    Changed name to adapter

--- structural/facade/complex_libraries/adapter/ItemToIngredientAdapter.java
+++ structural/facade/complex_libraries/adapter/ItemToIngredientAdapter.java
@@ -1,11 +1,11 @@
-package structural.facade.complex_libraries.translator;
+package structural.facade.complex_libraries.adapter;
 
 import structural.facade.complex_libraries.Ingredient;
 import structural.facade.complex_libraries.StockedIngredients;
 import structural.flyweight.Item;
 import structural.flyweight.ItemCount;
 
-public class ItemToIngredientTranslator {
+public class ItemToIngredientAdapter {
     public static StockedIngredients translate(ItemCount itemCount) {
         return new StockedIngredients(new Ingredient(itemCount.item.name), itemCount.count);
     }
